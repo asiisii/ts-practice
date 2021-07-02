5e6f9bc8796c4b401aba8ed9b645992004469312
Ashish Malla <ashishmalla45@gmail.com>
2021-07-02 09:01:45 -0600

    Add states and data types for all states

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { fetchAllMoviesData, checkForError } from '../../util/apiCalls'
+import { cleanAllMoviesData } from '../../util/cleanApiData'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface MoviesState {
+	movies: {
+		id: number
+		title: string
+		genres: string[]
+	}[]
 }
 
-export default App;
+const App: React.FC = () => {
+	const [allMovies, setAllMovies] = useState<MoviesState['movies']>([])
+	const [statusCode, setStatusCode] = useState<number>(200)
+	const [fetchedError, setFetchedError] = useState<boolean>(false)
+	const [error, setError] = useState<string>('')
+
+
+	console.log(allMovies)
+	return <div className='App'>app</div>
+}
+
+export default App
