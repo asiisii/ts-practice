2c7582eabec76018e6aecb3c7d3f09878598d861
Ashish Malla <ashishmalla45@gmail.com>
2021-07-02 08:50:33 -0600

    Add a function to return error msg using status code

--- src/util/apiCalls.ts
+++ src/util/apiCalls.ts
@@ -1,4 +1,5 @@
 let path: string = 'https://code-challenge.spectrumtoolbox.com/api/movie/'
+let errorMsg: string
 
 export const fetchAllMoviesData = async () => {
 	const res = await fetch(path, {
@@ -12,4 +13,18 @@ export const fetchAllMoviesData = async () => {
 }
 
 
+export const checkForError = (status: number): string => {
+	
+	switch (status) {
+		case 404:
+			errorMsg = "Sorry, we couldn't find repository you were looking for"
+			break
+		case 500:
+			errorMsg = 'Internal Server Error. Our whole team are now aware.'
+			break
+		default:
+			errorMsg = 'Oops! Request failed. Please try again.'
+	}
 
+	return errorMsg
+}
