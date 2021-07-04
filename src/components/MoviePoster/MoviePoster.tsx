05ee7f8421f5f58c0fc7f14fb0a091ccd6326e6c
Ashish Malla <ashishmalla45@gmail.com>
2021-07-04 12:05:19 -0600

    Remove unused defaultsrc function

--- src/components/MoviePoster/MoviePoster.tsx
+++ src/components/MoviePoster/MoviePoster.tsx
@@ -9,28 +9,16 @@ interface MoviePosterProps {
 	path: string
 }
 
+// ~~~ Displays all the movie posters with its title"
 export const MoviePoster: React.FC<MoviePosterProps> = ({
 	id,
 	title,
 	path,
 }) => {
-	const defaultSrc = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
-		// console.log(`original`)
-		if (e.target) {
-			console.log(e.target)
-		}
-
-		// e.target.src = `/assets/moviePosterImages/defaultImage.jpeg`
-
-		// console.log('changed')
-		// console.log(e.target.src)
-		// console.log(`-------------`)
-	}
-
 	return (
 		<div className='MoviePoster'>
 			<Link to={`/${id}`}>
-				<img onError={e => defaultSrc(e)} src={path} alt={`${title} poster`} />
+				<img src={path} alt={`${title} poster`} />
 				<h1 className='movie-title'>{title}</h1>
 			</Link>
 		</div>
