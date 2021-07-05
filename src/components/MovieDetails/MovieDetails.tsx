ba8893226ceeedceedee18243258d9273915e53e
Ashish Malla <ashishmalla45@gmail.com>
2021-07-05 11:29:25 -0600

    Comment out setAmovie method to remove the datatype error

--- src/components/MovieDetails/MovieDetails.tsx
+++ src/components/MovieDetails/MovieDetails.tsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import { RouteComponentProps } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { fetchMovieData, checkForError } from '../../util/apiCalls'
+import { cleanAMovieData } from '../../util/cleanApiData'
+import { AMovieData } from '../../util/dataTypes'
 
 interface MovieDetailsProps {
 	id: string
 }
 
-export const MovieDetails = ({
-	match,
-}: RouteComponentProps<MovieDetailsProps>) => {
-	return <div>{match.params.id}</div>
+export const MovieDetails: React.FC = () => {
+	const params = useParams<MovieDetailsProps>()
+	const [aMovie, setAMovie] = useState<AMovieData['movie']>()
+	const [statusCode, setStatusCode] = useState<number>(200)
+	const [fetchedError, setFetchedError] = useState<boolean>(false)
+
+	useEffect(() => {
+		getSingleMovieDetails()
+	}, [params.id])
+
+	const getSingleMovieDetails = async () => {
+		setFetchedError(false)
+		try {
+			const response = await fetchMovieData(`/${params.id}`)
+			setStatusCode(response.status)
+			const data = await response.json()
+
+			const cleanedData = cleanAMovieData(data.data)
+			console.log(cleanedData)
+			// setAMovie(cleanedData)
+		} catch (error) {
+			setFetchedError(true)
+		}
+	}
+	return <div>{aMovie}</div>
 }
