f702a15850de93b6f337cd9a1a8a22e19d49e446
Ashish Malla <ashishmalla45@gmail.com>
2021-07-05 02:53:51 -0600

    Update thhe class name from Navbar to logo-title-wrapper

--- cypress/integration/navbar_spec.js
+++ cypress/integration/navbar_spec.js
@@ -6,8 +6,8 @@ describe('Navbar', () => {
 	it('should have logo and title', () => {
 		cy.get('header')
 			.should('have.css', 'background-color', 'rgb(113, 201, 206)')
-			.get('.Navbar')
-			.should('have.css', 'max-width', '1440px')
+			.get('.logo-title-wrapper')
+			.should('have.css', 'max-width', '1350px')
 			.get('img')
 			.get('span')
 			.should('have.text', 'ChartFlix')
