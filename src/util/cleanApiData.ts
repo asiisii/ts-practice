d7c10d31e7b5d68d2843c006fa851f30861f8eff
Ashish Malla <ashishmalla45@gmail.com>
2021-07-05 12:37:10 -0600

    Format seconds to hr:mm:ss format

--- src/util/cleanApiData.ts
+++ src/util/cleanApiData.ts
@@ -1,4 +1,4 @@
-import { MoviesState as MoviesData, AMovieData } from './dataTypes'
+import { MoviesState as MoviesData, AMovieApiData } from './dataTypes'
 
 //~~~ Returns movies data after iterating through all movies data ~~~
 export const cleanAllMoviesData = (moviesData: MoviesData['movies']) => {
@@ -31,14 +31,28 @@ const tryRequire = (path: string) => {
 }
 
 // ~~~ Returns only necessary data of clicked movie poster ~~~
-export const cleanAMovieData = (aMovieData: AMovieData['movie']) => {
-  console.log(aMovieData);
-  return {
+export const cleanAMovieData = (aMovieData: AMovieApiData) => {
+	console.log(aMovieData)
+	const movieGenres = aMovieData.genres.join(' | ')
+	const time = new Date(aMovieData.duration * 1000).toISOString().substr(11, 8)
+	console.log(time)
+	const givenSecs = aMovieData.duration
+	var hr = Math.floor(givenSecs / 3600)
+	var min = Math.floor((givenSecs % 3600) / 60)
+	var sec = Math.floor((givenSecs % 3600) % 60)
+
+	var formattedHr = hr > 0 ? hr + (hr === 1 ? 'hr' : 'hrs') : ''
+	let formattedMin = min > 0 ? ':' + min + (min === 1 ? 'min' : 'mins') : ''
+	var formattedSec =
+		(sec > 0 ? ':' + sec + (sec === 1 ? 'sec' : 'secs') : '') || (sec < 1 && '')
+	const displayDuration = `${formattedHr}${formattedMin}${formattedSec}`
+
+	return {
 		id: aMovieData.id,
 		title: aMovieData.title,
 		releaseYear: aMovieData.releaseYear,
-		duration: aMovieData.duration,
-		genres: aMovieData.genres,
+		duration: displayDuration,
+		genres: movieGenres,
 		description: aMovieData.description,
 		topCast: aMovieData.topCast,
 	}
