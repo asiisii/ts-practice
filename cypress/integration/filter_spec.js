01ebe46e4291bab47d14f6659347884c60258978
Ashish Malla <ashishmalla45@gmail.com>
2021-07-05 02:21:33 -0600

    Add test to check search is filtering

--- cypress/integration/filter_spec.js
+++ cypress/integration/filter_spec.js
@@ -1,10 +1,10 @@
 describe('Filter', () => {
-  beforeEach(() => {
+	beforeEach(() => {
 		cy.interceptAllMoviesFetches()
 	})
 
-  it('should have search bar and genre options', () => {
-    cy.get('input[name="search"]')
+	it('should have search bar and genre options', () => {
+		cy.get('input[name="search"]')
 			.should('be.visible')
 			.get('select')
 			.find('.select-items')
@@ -15,5 +15,16 @@ describe('Filter', () => {
 			.get('.select-items')
 			.eq(1)
 			.should('have.text', 'Action')
-  })
+	})
+
+	it.only('should be able to search by text', () => {
+		cy.get('input[name="search"]')
+			.should('have.value', '')
+			.type('Riders')
+			.should('have.value', 'riders')
+			.get('.movies')
+			.find('.MoviePoster')
+			.should('have.length', 1)
+			.should('be.visible')
+	})
 })
