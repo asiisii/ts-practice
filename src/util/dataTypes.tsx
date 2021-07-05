f1decbbbb9d8d33b6809e6255c5f85719d6ed68b
Ashish Malla <ashishmalla45@gmail.com>
2021-07-05 12:43:19 -0600

    Add interface for MovieDetailsProps

--- src/util/dataTypes.tsx
+++ src/util/dataTypes.tsx
@@ -7,17 +7,32 @@ export interface MoviesState {
 	}[]
 }
 
-export interface AMovieData {
-	movie: {
-		id: string
-		title: string
-		releaseYear: number
-		duration: number
-		genres: string[]
-		description: string
-		topCast: {
-			name: string
-			characterName: string
-		}[]
-	}
+export interface AMovieApiData {
+	id: string
+	title: string
+	releaseYear: number
+	duration: number
+	genres: string[]
+	description: string
+	topCast: {
+		name: string
+		characterName: string
+	}[]
+}
+
+export interface AMovieState {
+	id: string
+	title: string
+	releaseYear: number
+	duration: string
+	genres: string
+	description: string
+	topCast: {
+		name: string
+		characterName: string
+	}[]
+}
+
+export interface MovieDetailsProps {
+	id: string
 }
