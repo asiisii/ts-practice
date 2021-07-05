3dd959938780f5995ffb8ce3a7286a5af63b2bf6
Ashish Malla <ashishmalla45@gmail.com>
2021-07-05 12:44:10 -0600

    Assign cleanedData to amovie state

--- src/components/MovieDetails/MovieDetails.tsx
+++ src/components/MovieDetails/MovieDetails.tsx
@@ -2,15 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { fetchMovieData, checkForError } from '../../util/apiCalls'
 import { cleanAMovieData } from '../../util/cleanApiData'
-import { AMovieData } from '../../util/dataTypes'
-
-interface MovieDetailsProps {
-	id: string
-}
+import { AMovieState, MovieDetailsProps } from '../../util/dataTypes'
 
 export const MovieDetails: React.FC = () => {
 	const params = useParams<MovieDetailsProps>()
-	const [aMovie, setAMovie] = useState<AMovieData['movie']>()
+	const [aMovie, setAMovie] = useState<AMovieState>()
 	const [statusCode, setStatusCode] = useState<number>(200)
 	const [fetchedError, setFetchedError] = useState<boolean>(false)
 
@@ -24,13 +20,11 @@ export const MovieDetails: React.FC = () => {
 			const response = await fetchMovieData(`/${params.id}`)
 			setStatusCode(response.status)
 			const data = await response.json()
-
 			const cleanedData = cleanAMovieData(data.data)
-			console.log(cleanedData)
-			// setAMovie(cleanedData)
+			setAMovie(cleanedData)
 		} catch (error) {
 			setFetchedError(true)
 		}
 	}
-	return <div>{aMovie}</div>
+	return <div>hello</div>
 }
