83ee85663d511affb2f79c2e976ee409d1c0d01d
Ashish Malla <ashishmalla45@gmail.com>
2021-07-05 21:50:45 -0600

    Add test to see if movie details are visible or not

--- cypress/integration/movieDetails_spec.js
+++ cypress/integration/movieDetails_spec.js
@@ -20,7 +20,7 @@ describe('MoviePosters', () => {
 			.click()
 	})
 
-	it.only('should be able to see both poster and hero images', () => {
+	it('should be able to see both poster and hero images', () => {
     cy.get('img')
 			.eq(2)
 			.should('have.attr', 'src')
@@ -29,5 +29,25 @@ describe('MoviePosters', () => {
 			.eq(3)
 			.should('have.attr', 'src')
 			.should('equal', '/assets/movieHeroImages/SP013727990000.jpeg')
+  })
+
+  it('should be able to see all the movie details', () => {
+    cy.get('.movie-title')
+			.should('have.text', 'Riders of Justice')
+			.get('.release-year')
+			.should('have.text', 2020)
+			.get('.duration')
+			.should('have.text', '1hr:56mins')
+			.get('.genres')
+			.should('contain', 'Action')
+			.get('.description')
+			.should(
+				'contain',
+				'Markus returns home to care for his daughter when his wife'
+			)
+			.get('.top-cast')
+			.should('contain', 'Mads Mikkelsen')
+			.get('.top-cast')
+			.should('contain', 'Andrea Heick Gadeberg')
   })
 })
