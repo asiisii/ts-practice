6af19023d18adce1574c92bc7d57ceebe9b33f3f
Ashish Malla <ashishmalla45@gmail.com>
2021-07-05 22:49:22 -0600

    Add test for 500 status code

--- cypress/integration/error_spec.js
+++ cypress/integration/error_spec.js
@@ -38,4 +38,26 @@ describe('Error handling', () => {
 				"We're sorry, but you've sent too many requests to us recently. Please try again later."
 			)
 	})
+
+  it('should display error message for 404 status code', () => {
+		const baseURL = 'https://code-challenge.spectrumtoolbox.com/api/movies'
+		cy.intercept(
+			{
+				method: 'GET',
+				url: `${baseURL}`,
+				headers: {
+					Authorization: 'Api-Key q3MNxtfep8Gt',
+				},
+			},
+			{
+				statusCode: 500,
+			}
+		)
+			.visit('http://localhost:3000/')
+			.get('.err-msg')
+			.should(
+				'have.text',
+				'Internal Server Error. Our whole team are now aware.'
+			)
+	})
 })
