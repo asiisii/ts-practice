6bcfd2548d19d2dddd0d580ed3ba11c7185d636b
Ashish Malla <ashishmalla45@gmail.com>
2021-07-05 22:56:12 -0600

    Add test to test no movies found error message

--- cypress/integration/error_spec.js
+++ cypress/integration/error_spec.js
@@ -1,82 +1,98 @@
 describe('Error handling', () => {
-  
-  it('should display error message for 404 status code', () => {
-    const baseURL = 'https://code-challenge.spectrumtoolbox.com/api/movies' 
-    cy.intercept(
-			{
-				method: 'GET',
-				url: `${baseURL}`,
-				headers: {
-					Authorization: 'Api-Key q3MNxtfep8Gt',
+	describe('Api errors', () => {
+		it('should display error message for 404 status code', () => {
+			const baseURL = 'https://code-challenge.spectrumtoolbox.com/api/movies'
+			cy.intercept(
+				{
+					method: 'GET',
+					url: `${baseURL}`,
+					headers: {
+						Authorization: 'Api-Key q3MNxtfep8Gt',
+					},
 				},
-			},
-			{
-				statusCode: 404,
-			}
-		).visit('http://localhost:3000/')
-    .get('.err-msg').should('have.text', 'Sorry, we couldn\'t find movies you were looking for')
+				{
+					statusCode: 404,
+				}
+			)
+				.visit('http://localhost:3000/')
+				.get('.err-msg')
+				.should(
+					'have.text',
+					"Sorry, we couldn't find movies you were looking for"
+				)
+		})
+
+		it('should display error message for 429 status code', () => {
+			const baseURL = 'https://code-challenge.spectrumtoolbox.com/api/movies'
+			cy.intercept(
+				{
+					method: 'GET',
+					url: `${baseURL}`,
+					headers: {
+						Authorization: 'Api-Key q3MNxtfep8Gt',
+					},
+				},
+				{
+					statusCode: 429,
+				}
+			)
+				.visit('http://localhost:3000/')
+				.get('.err-msg')
+				.should(
+					'have.text',
+					"We're sorry, but you've sent too many requests to us recently. Please try again later."
+				)
+		})
+
+		it('should display error message for 500 status code', () => {
+			const baseURL = 'https://code-challenge.spectrumtoolbox.com/api/movies'
+			cy.intercept(
+				{
+					method: 'GET',
+					url: `${baseURL}`,
+					headers: {
+						Authorization: 'Api-Key q3MNxtfep8Gt',
+					},
+				},
+				{
+					statusCode: 500,
+				}
+			)
+				.visit('http://localhost:3000/')
+				.get('.err-msg')
+				.should(
+					'have.text',
+					'Internal Server Error. Our whole team are now aware.'
+				)
+		})
+
+		it('should display error message for any other error status code', () => {
+			const baseURL = 'https://code-challenge.spectrumtoolbox.com/api/movies'
+			cy.intercept(
+				{
+					method: 'GET',
+					url: `${baseURL}`,
+					headers: {
+						Authorization: 'Api-Key q3MNxtfep8Gt',
+					},
+				},
+				{
+					statusCode: 401,
+				}
+			)
+				.visit('http://localhost:3000/')
+				.get('.err-msg')
+				.should('have.text', 'Oops! Request failed. Please try again.')
+		})
+	})
+
+  describe('Filter movie error', () => {
+    it.only('should say no movies found if search doesn\'t match', () => {
+      cy.interceptAllMoviesFetches()
+				.get('input[name="search"]')
+				.type('asd')
+				.should('have.value', 'asd')
+				.get('.err-msg').should('have.text', 'No movies found.')
+    })
   })
-
-  it('should display error message for 429 status code', () => {
-		const baseURL = 'https://code-challenge.spectrumtoolbox.com/api/movies'
-		cy.intercept(
-			{
-				method: 'GET',
-				url: `${baseURL}`,
-				headers: {
-					Authorization: 'Api-Key q3MNxtfep8Gt',
-				},
-			},
-			{
-				statusCode: 429,
-			}
-		)
-			.visit('http://localhost:3000/')
-			.get('.err-msg')
-			.should(
-				'have.text',
-				"We're sorry, but you've sent too many requests to us recently. Please try again later."
-			)
-	})
-
-  it('should display error message for 500 status code', () => {
-		const baseURL = 'https://code-challenge.spectrumtoolbox.com/api/movies'
-		cy.intercept(
-			{
-				method: 'GET',
-				url: `${baseURL}`,
-				headers: {
-					Authorization: 'Api-Key q3MNxtfep8Gt',
-				},
-			},
-			{
-				statusCode: 500,
-			}
-		)
-			.visit('http://localhost:3000/')
-			.get('.err-msg')
-			.should(
-				'have.text',
-				'Internal Server Error. Our whole team are now aware.'
-			)
-	})
-
-  it('should display error message for any other error status code', () => {
-		const baseURL = 'https://code-challenge.spectrumtoolbox.com/api/movies'
-		cy.intercept(
-			{
-				method: 'GET',
-				url: `${baseURL}`,
-				headers: {
-					Authorization: 'Api-Key q3MNxtfep8Gt',
-				},
-			},
-			{
-				statusCode: 401,
-			}
-		)
-			.visit('http://localhost:3000/')
-			.get('.err-msg')
-			.should('have.text', 'Oops! Request failed. Please try again.')
-	})
 })
