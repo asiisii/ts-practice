c8e0cba8aaf4ae5247f7fc83e5e6c4c19b23e23d
Ashish Malla <ashishmalla45@gmail.com>
2021-07-05 21:40:27 -0600

    Add test to check correct images were getting displayed

--- cypress/integration/movieDetails_spec.js
+++ cypress/integration/movieDetails_spec.js
@@ -1,12 +1,33 @@
 describe('MoviePosters', () => {
 	beforeEach(() => {
 		cy.interceptAllMoviesFetches()
-		cy.interceptSingleMovieFetch()
+			.interceptSingleMovieFetch()
+			.get('.movies')
+			.find('.MoviePoster')
+			.get('img')
+			.eq(1)
+			.click()
 	})
 
 	it('should be able to toggle between details and home page', () => {
-		cy.get('.movies').find('.MoviePoster').get('img').eq(1).click()
-    .get('header').find('.logo-title-wrapper')
-    .find('img').get('span').should('have.text', 'ChartFlix').get('a').find('img').click()
+		cy.get('header')
+			.find('.logo-title-wrapper')
+			.find('img')
+			.get('span')
+			.should('have.text', 'ChartFlix')
+			.get('a')
+			.find('img')
+			.click()
 	})
+
+	it.only('should be able to see both poster and hero images', () => {
+    cy.get('img')
+			.eq(2)
+			.should('have.attr', 'src')
+			.should('equal', '/assets/moviePosterImages/SP013727990000.jpeg')
+			.get('img')
+			.eq(3)
+			.should('have.attr', 'src')
+			.should('equal', '/assets/movieHeroImages/SP013727990000.jpeg')
+  })
 })
