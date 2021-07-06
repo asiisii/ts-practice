5f6adfdef6b46f2460e117899c89ed6d414cba61
Ashish Malla <ashishmalla45@gmail.com>
2021-07-05 22:48:32 -0600

    Add test for 429 status code

--- cypress/integration/error_spec.js
+++ cypress/integration/error_spec.js
@@ -16,4 +16,26 @@ describe('Error handling', () => {
 		).visit('http://localhost:3000/')
     .get('.err-msg').should('have.text', 'Sorry, we couldn\'t find movies you were looking for')
   })
+
+  it('should display error message for 404 status code', () => {
+		const baseURL = 'https://code-challenge.spectrumtoolbox.com/api/movies'
+		cy.intercept(
+			{
+				method: 'GET',
+				url: `${baseURL}`,
+				headers: {
+					Authorization: 'Api-Key q3MNxtfep8Gt',
+				},
+			},
+			{
+				statusCode: 429,
+			}
+		)
+			.visit('http://localhost:3000/')
+			.get('.err-msg')
+			.should(
+				'have.text',
+				"We're sorry, but you've sent too many requests to us recently. Please try again later."
+			)
+	})
 })
